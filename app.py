ab76e3049253978cbe72c3f726146b73390e45ec
YuHyungmin1226 <yu.hyungmin1226@gmail.com>
2026-05-02 21:16:54 +0900

    Update CSP to allow direct connections to Google APIs

--- app.py
+++ app.py
@@ -62,7 +62,8 @@ def create_app():
         'style-src': ['\'self\'', 'https://cdn.jsdelivr.net', 'https://fonts.googleapis.com', '\'unsafe-inline\''],
         'font-src': ['\'self\'', 'https://cdn.jsdelivr.net', 'https://fonts.gstatic.com'],
         'img-src': ['\'self\'', 'data:', 'https://*.googleusercontent.com', 'https://drive.google.com', 'https://*.youtube.com', 'https://*.ytimg.com', 'https://img.youtube.com', 'https://i.ytimg.com'],
-        'frame-src': ['\'self\'', 'https://drive.google.com', 'https://*.youtube.com', 'https://www.youtube.com', 'https://youtube.com']
+        'frame-src': ['\'self\'', 'https://drive.google.com', 'https://*.youtube.com', 'https://www.youtube.com', 'https://youtube.com'],
+        'connect-src': ['\'self\'', 'https://www.googleapis.com', 'https://cdn.jsdelivr.net']
     }
     Talisman(app, content_security_policy=csp, force_https=False)
 
